82344d0c20d829266bb64f5e491a6ec11300e1f7
David Uebelacker <david.uebelacker@endress.com>
2022-11-02 21:04:16 +0100

    :art: added tailwindcss

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    join(__dirname, '../../libs/ui/src/**/!(*.stories|*.spec).{ts,html}'),
+  ]
+};
