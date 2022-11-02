db5001aeb04af1a49b16297147d0a02541ebe2e8
David Uebelacker <david.uebelacker@endress.com>
2022-11-02 22:37:06 +0100

    :ok_hand: fixed failing test

--- apps/frontend/specs/index.spec.tsx
+++ apps/frontend/specs/index.spec.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
+import { useQuery } from '@apollo/client';
 import Index from '../pages/index';
+
+jest.mock('@apollo/client', () => ({ useQuery: jest.fn(), gql: jest.fn() }));
 
 describe('Index', () => {
   it('should render successfully', () => {
+    (useQuery as jest.Mock).mockReturnValue({ data: { app: { serverTime: '2022' } } });
     const { baseElement } = render(<Index />);
     expect(baseElement).toBeTruthy();
   });
