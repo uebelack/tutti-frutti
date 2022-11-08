0b1248f8778f72dc253b946417d2e6baf0b64533
David Uebelacker <david.uebelacker@endress.com>
2022-11-08 11:01:30 +0100

    :ok_hand: trying to fix prod config

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -12,6 +12,9 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  env: {
+    NEXT_PUBLIC_GRAPHQL_API_URL: process.env.NEXT_PUBLIC_GRAPHQL_API_URL
+  }
 };
 
 module.exports = withNx(nextConfig);
