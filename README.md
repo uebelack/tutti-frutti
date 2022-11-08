0a33d22630aadcdd4c86d6623a960c7d5d0008f1
Mateusz Juszczyk <matixovi@gmail.com>
2022-11-08 23:30:06 +0100

    :tada: added prisma

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 ## Get started
 
-1. yarn install
-2. yarn start
-3. open http://localhost:4200/
+1. Copy the `.env.example` file to `.env`
+2. `yarn install`
+3. `yarn start`
+4. open http://localhost:4200/
 
+## Database
 
+1. Run `docker-compose up` to set up database container locally
 
- 
+### Prisma scripts
+
+`yarn prisma:migrate-dev` - run migrations in development mode
+`yarn prisma:studio` - open prisma studio
--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to backend!' };
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getData(): Promise<{ message: string; users: User[] }> {
+    const users = await this.prisma.user.findMany();
+    return { message: 'Welcome to backend!', users };
   }
 }
