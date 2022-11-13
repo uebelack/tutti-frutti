f19d6ba68170df40d6f4543b13e6385a91c3fcc9
David Uebelacker <david.uebelacker@endress.com>
2022-11-13 11:40:51 +0100

    :tada: added tuturial page, small bug fix

---
+++ apps/frontend/pages/tutorial/index.tsx
@@ -0,0 +1,17 @@
+export const Login = () => (
+  <article className="prose">
+      <h1>Tutorial</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id malesuada lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+          In ac hendrerit quam. Nulla facilisi. Nam venenatis dictum neque eu semper. Morbi elementum orci sed leo cursus, sit amet auctor urna rutrum.
+          Aenean vel eros condimentum, mollis orci ac, tincidunt orci.
+          Donec risus metus, mollis eleifend libero vitae, vehicula bibendum eros. Curabitur tristique, ipsum sit amet luctus dapibus, elit sem rhoncus augue,
+           non malesuada felis magna sed orci. Integer ut augue nec nulla blandit placerat non a nunc. Phasellus vehicula, tortor non porta scelerisque, ipsum nisi tempus velit</p>
+        <p>Praesent venenatis, nisl id convallis laoreet, nunc lectus lacinia lorem, ac pretium augue orci et libero. Quisque et consequat tellus.
+          Donec viverra blandit tortor, sed congue felis semper et. Praesent mattis nibh tempus odio auctor faucibus. Mauris orci ipsum, viverra vitae tempus ac,
+          molestie vel tellus.</p>
+        <p>Nullam ultrices nisi erat, vel bibendum arcu dignissim sit amet. Nunc blandit lacinia orci eget porttitor. Quisque ultrices tempor ultrices.
+          Phasellus quis aliquam libero, sed ultricies nisl. Nullam vulputate, massa ac varius elementum, diam orci mollis urna, ut egestas leo tortor ut augue. </p>
+    </article>
+  );
+
+export default Login;
